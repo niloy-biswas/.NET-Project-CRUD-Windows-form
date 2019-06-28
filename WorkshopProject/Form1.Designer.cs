0aa3a197068e0007d3f23af790bc6b9f78141c1a
Niloy Biswas <niloybiswas1999@gmail.com>
2019-06-28 19:47:12 +0600

    Add files via upload

---
+++ WorkshopProject/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WorkshopProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_address = new System.Windows.Forms.TextBox();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Save = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.tb_search = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.update = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_name
+            // 
+            this.tb_name.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tb_name.Location = new System.Drawing.Point(210, 72);
+            this.tb_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(188, 22);
+            this.tb_name.TabIndex = 0;
+            // 
+            // tb_address
+            // 
+            this.tb_address.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tb_address.Location = new System.Drawing.Point(210, 161);
+            this.tb_address.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_address.Name = "tb_address";
+            this.tb_address.Size = new System.Drawing.Size(188, 22);
+            this.tb_address.TabIndex = 1;
+            // 
+            // tb_email
+            // 
+            this.tb_email.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tb_email.Location = new System.Drawing.Point(210, 117);
+            this.tb_email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(188, 22);
+            this.tb_email.TabIndex = 2;
+            this.tb_email.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(661, 27);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(655, 393);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Save.Location = new System.Drawing.Point(210, 202);
+            this.Save.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 34);
+            this.Save.TabIndex = 8;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Delete.Location = new System.Drawing.Point(274, 252);
+            this.Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 29);
+            this.Delete.TabIndex = 9;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // tb_search
+            // 
+            this.tb_search.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tb_search.Location = new System.Drawing.Point(210, 346);
+            this.tb_search.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_search.Name = "tb_search";
+            this.tb_search.Size = new System.Drawing.Size(188, 22);
+            this.tb_search.TabIndex = 12;
+            // 
+            // Search
+            // 
+            this.Search.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Search.Location = new System.Drawing.Point(274, 387);
+            this.Search.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 33);
+            this.Search.TabIndex = 13;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = false;
+            this.Search.Click += new System.EventHandler(this.Search_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 351);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Search by name";
+            // 
+            // tb_id
+            // 
+            this.tb_id.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tb_id.Location = new System.Drawing.Point(210, 33);
+            this.tb_id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(188, 22);
+            this.tb_id.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(171, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "ID";
+            // 
+            // update
+            // 
+            this.update.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.update.Location = new System.Drawing.Point(323, 202);
+            this.update.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 34);
+            this.update.TabIndex = 19;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = false;
+            this.update.Click += new System.EventHandler(this.update_Click_1);
+            // 
+            // refresh
+            // 
+            this.refresh.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.refresh.Location = new System.Drawing.Point(952, 461);
+            this.refresh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(84, 34);
+            this.refresh.TabIndex = 20;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1347, 551);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.tb_search);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_address);
+            this.Controls.Add(this.tb_name);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "`";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_address;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox tb_search;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ WorkshopProject/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WorkshopProject
+{ 
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = new SqlConnection();
+        private string conString = "Server = NILOY; Database = Workshop; User = sa; password = 1234";
+        private SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        //for showing value in gridview
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            conn.ConnectionString = conString;
+
+            cmd = conn.CreateCommand();
+            try
+            {
+                string query = "sp_ShowAllData";
+                cmd.CommandText = query;
+                conn.Open();
+
+
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                reader.Close();
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                dataGridView1.ClearSelection();
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+
+        }
+        //click and show value
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            tb_id.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            tb_name.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            tb_email.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            tb_address.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+        //rafresh all
+
+        private void refresh_Click_1(object sender, EventArgs e)
+        {
+            Form1_Load_1(this, e);
+            tb_id.Clear();
+            tb_name.Clear();
+            tb_email.Clear();
+            tb_address.Clear();
+            tb_search.Clear();
+        }
+        //insert value
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+            string name = tb_name.Text;
+            string address = tb_address.Text;
+            string email = tb_email.Text;
+            string id = tb_id.Text;
+
+            if ((name == "") || (address == "") || (email == ""))
+            {
+                string msg = "No text box can be empty";
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                conn.ConnectionString = conString;
+                cmd = conn.CreateCommand();
+                try
+                {
+                    string query = "sp_insertMember";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@id", SqlDbType.VarChar);
+                    cmd.Parameters["@id"].Value = id;
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar);
+                    cmd.Parameters["@name"].Value = name;
+                    cmd.Parameters.Add("@address", SqlDbType.VarChar);
+                    cmd.Parameters["@address"].Value = address;
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar);
+                    cmd.Parameters["@email"].Value = email;
+                    cmd.CommandText = query;
+                    conn.Open();
+                    cmd.ExecuteScalar();
+                    MessageBox.Show("Value Successfully saved\n" + "Your ID: " + tb_id.Text + "\nYour Name: " + tb_name.Text + "\nGmail: " + tb_address.Text + "\nAddress: " + tb_email.Text);
+                    dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                    tb_id.Clear();
+                    tb_name.Clear();
+                    tb_email.Clear();
+                    tb_address.Clear();
+                    tb_search.Clear();
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+
+                Form1_Load_1(this, e);
+
+
+            }
+        }
+
+        //value update
+        private void update_Click_1(object sender, EventArgs e)
+        {
+            string name = tb_name.Text;
+            string address = tb_address.Text;
+            string email = tb_email.Text;
+            string id = tb_id.Text;
+            string updateid = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+            if ((name == "") || (address == "") || (email == ""))
+            {
+                string msg = "No text box can be empty";
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                conn.ConnectionString = conString;
+                cmd = conn.CreateCommand();
+                try
+                {
+                    string query = "sp_update";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@updateid", SqlDbType.VarChar);
+                    cmd.Parameters["@updateid"].Value = updateid;
+                    cmd.Parameters.Add("@name", SqlDbType.VarChar);
+                    cmd.Parameters["@name"].Value = name;
+                    cmd.Parameters.Add("@address", SqlDbType.VarChar);
+                    cmd.Parameters["@address"].Value = address;
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar);
+                    cmd.Parameters["@email"].Value = email;
+                    cmd.CommandText = query;
+                    conn.Open();
+                    cmd.ExecuteScalar();
+                    MessageBox.Show("Value of ID - "+updateid+" Updated successfully");
+                    dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                    tb_id.Clear();
+                    tb_name.Clear();
+                    tb_email.Clear();
+                    tb_address.Clear();
+                    tb_search.Clear();
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+
+                Form1_Load_1(this, e);
+
+
+            }
+        }
+
+        //value delete
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+            String n = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            conn.ConnectionString = conString;
+            cmd = conn.CreateCommand();
+            try
+            {
+                string query = "sp_delete";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@n", SqlDbType.VarChar);
+                cmd.Parameters["@n"].Value = n;
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+                MessageBox.Show("Value of "+n+" deleted successfully");
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                tb_id.Clear();
+                tb_name.Clear();
+                tb_email.Clear();
+                tb_address.Clear();
+                tb_search.Clear();
+            }
+            catch (Exception ex)
+            { 
+                string msg = ex.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+
+            Form1_Load_1(this, e);
+        }
+    
+
+         
+        //value search
+
+        private void Search_Click_1(object sender, EventArgs e)
+        {
+            String s = tb_search.Text;
+
+            conn.ConnectionString = conString;
+            cmd = conn.CreateCommand();
+            try
+            {
+                string query = "sp_search";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@s", SqlDbType.VarChar);
+                cmd.Parameters["@s"].Value = s;
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                reader.Close();
+                dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+                tb_search.Clear();
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+
+        
+    }
+}
+    
+    
